52e2735d742cd3b8dc3d61cfb6157d5e315a0862
Kai-Uwe Bux <bux_gitlab@kubux.net>
2025-12-21 12:25:41 +0100

    bug hunting in flake.nix

--- flake.nix
+++ flake.nix
@@ -79,9 +79,9 @@
               makeWrapper ${pythonEnv}/bin/python $out/bin/$file \
                 --add-flags "$out/bin/$file.py" \
                 --set-default TMPDIR "/tmp" \
+		;
                 # --prefix LD_LIBRARY_PATH : "${pkgs.lib.makeLibraryPath [ pkgs.xorg.xcbutilcursor pkgs.xorg.libX11 pkgs.libxkbcommon pkgs.dbus.lib]}" \
                 # --set QT_PLUGIN_PATH "${pkgs.qt6.qtbase}/${pkgs.qt6.qtbase.qtPluginPrefix}" \
-		;
 	          done
     
             # Copy desktop file
