e9542c4df0bf2ec522d39dd6b6321a2b49f63692
Kai-Uwe Bux <bux_gitlab@kubux.net>
2026-04-06 22:12:56 +0200

    hmpf

--- scripts/ai-train.py
+++ scripts/ai-train.py
@@ -110,10 +110,10 @@ def main():
             file = f"{msg["filename"][0]}"
             new_tagged_data[file] = tags
         
-        # vectorizer = TfidfVectorizer(ngram_range=(1, 5), stop_words=None, min_df=5, max_df=0.8)
-        vectorizer = TfidfVectorizer(ngram_range=(1, 4), stop_words=None, min_df=5, max_df=0.8)
-        # classifier = OneVsRestClassifier(LinearSVC(C=1.0, dual=True, max_iter=1000))
-        classifier = OneVsRestClassifier(MLPClassifier(hidden_layer_sizes=(100,100), verbose=True))
+        vectorizer = TfidfVectorizer(ngram_range=(1, 5), stop_words=None, min_df=5, max_df=0.8)
+        # vectorizer = TfidfVectorizer(ngram_range=(1, 4), stop_words=None, min_df=5, max_df=0.8)
+        classifier = OneVsRestClassifier(LinearSVC(C=1.0, dual=True, max_iter=1000))
+        # classifier = OneVsRestClassifier(MLPClassifier(hidden_layer_sizes=(100), verbose=True))
 
     # --- Data Preparation and Training (common to both modes) ---
     X_data = []
